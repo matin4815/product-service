5eb2bb98b3e8c2c6242bbbe98363f9e1cc8a52a8
matin <m.nazari@fakour.net>
2023-06-25 10:19:41 +0330

    a comment was added

--- src/main/java/com/matin/productservice/feign/CheckUserPurchase.java
+++ src/main/java/com/matin/productservice/feign/CheckUserPurchase.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CheckUserPurchase {
 
+    // a hard coded api, that represents the call to a customer management
     public Boolean userPurchaseValidation(Long productId, String username) {
         return false;
     }
